f18057a14841e739bcfef41de621e9cea2473940
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-01-29 18:37:07 +0900

    add parser test

--- test_hundler.py
+++ test_hundler.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+from dateutil.tz import gettz as dateutil_gettz
 from dotenv import load_dotenv
-from handler import RequestChalenge, Response  # 型の読み込み
+from handler import RequestChalenge, Response, Entry  # 型の読み込み
 import handler  # 関数の読み込み
 import os
 
@@ -12,19 +14,55 @@ HMAC_SECRET: str = os.environ["PuSH_hmac_secret"]
 
 
 def test_challenge():
-    expected = Response(200, VERIFY_TOKEN)
-    actual = handler.challenge(RequestChalenge(VERIFY_TOKEN))
+    challenge = "challenge"
+    expected = Response(200, challenge)
+    actual = handler.challenge(RequestChalenge(VERIFY_TOKEN, challenge))
     assert expected == actual
 
     expected = 404
-    actual = handler.challenge(RequestChalenge("hoge"))
+    actual = handler.challenge(RequestChalenge("hoge", challenge))
     assert expected == actual.statusCode
 
     expected = 404
-    actual = handler.challenge(RequestChalenge(""))
+    actual = handler.challenge(RequestChalenge("", challenge))
     assert expected == actual.statusCode
 
 
 def test_validate_hmac():
     assert handler.validate_hmac("0c94515c15e5095b8a87a50ba0df3bf38ed05fe6", "test", "test")
     assert not handler.validate_hmac("1c94515c15e5095b8a87a50ba0df3bf38ed05fe6", "test", "test")
+
+
+def test_parse():
+    expected = Entry(
+        videoId="test_videoId",
+        channelId="test_channelId",
+        title="test_title",
+        link="http://example.com/watch?v=test_videoId",
+        authorName="test_author_name",
+        authorUri="http://example.com/channel/test_channelId",
+        published=datetime(2022, 1, 28, 21, 40, 35).astimezone(dateutil_gettz('Asia/Tokyo')),
+        updated=datetime(2022, 1, 28, 21, 40, 58, 132027).astimezone(dateutil_gettz('Asia/Tokyo'))
+    )
+    actual = handler.parse("""<?xml version='1.0' encoding='UTF-8'?>
+<feed xmlns:yt="http://www.youtube.com/xml/schemas/2015" xmlns="http://www.w3.org/2005/Atom">
+    <link rel="hub" href="https://pubsubhubbub.appspot.com" />
+    <link rel="self" href="https://www.youtube.com/xml/feeds/videos.xml?channel_id=test_channelId" />
+    <title>YouTube video feed</title>
+    <updated>2022-01-28T12:40:58.132027669+00:00</updated>
+    <entry>
+        <id>yt:video:test_videoId</id>
+        <yt:videoId>test_videoId</yt:videoId>
+        <yt:channelId>test_channelId</yt:channelId>
+        <title>test_title</title>
+        <link rel="alternate" href="http://example.com/watch?v=test_videoId" />
+        <author>
+            <name>test_author_name</name>
+            <uri>http://example.com/channel/test_channelId</uri>
+        </author>
+        <published>2022-01-28T12:40:35+00:00</published>
+        <updated>2022-01-28T12:40:58.132027669+00:00</updated>
+    </entry>
+</feed>
+""")
+    assert expected == actual
